ffff52e0476f8bf46ed20f6e3675c8eb096b01ff
Hemalathaa2 <155246186+Hemalathaa2@users.noreply.github.com>
2026-04-15 16:33:53 +0530

    Update api.py

--- api.py
+++ api.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI, UploadFile, File, Form
 from typing import List
 from utils import *
-from database import init_db, insert_result
-from fastapi.middleware.cors import CORSMiddleware
-init_db()
+
 app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"status": "API Running"}
 
 @app.post("/analyze/")
 async def analyze(files: List[UploadFile] = File(...), jd_text: str = Form(...)):
@@ -14,16 +16,19 @@ async def analyze(files: List[UploadFile] = File(...), jd_text: str = Form(...))
     texts, names = [], []
 
     for f in files:
-        if f.filename.endswith(".pdf"):
-            text = extract_text_from_pdf(f.file)
-        else:
-            text = extract_text_from_docx(f.file)
+        try:
+            if f.filename.endswith(".pdf"):
+                text = extract_text_from_pdf(f.file)
+            else:
+                text = extract_text_from_docx(f.file)
 
-        clean = preprocess(text)
+            clean = preprocess(text)
 
-        if len(clean) > 50:
-            texts.append(clean)
-            names.append(f.filename)
+            if len(clean) > 50:
+                texts.append(clean)
+                names.append(f.filename)
+        except:
+            continue
 
     jd_emb = get_embeddings_batch([jd_clean])[0]
     res_embs = get_embeddings_batch(texts)
@@ -32,23 +37,16 @@ async def analyze(files: List[UploadFile] = File(...), jd_text: str = Form(...))
 
     for i in range(len(texts)):
         score = compute_detailed_score(jd_clean, texts[i], jd_emb, res_embs[i])
-        result = {"name": names[i], **score}
-        insert_result(result)   # ✅ store in DB
-        results.append(result)
+        results.append({"name": names[i], **score})
+
     results.sort(key=lambda x: x["final_score"], reverse=True)
 
-    if results:
-        results[0]["llm_explanation"] = generate_explanation(
-            jd_text, texts[0], results[0]
+    # AI explanation (top 3 only for speed)
+    for i in range(min(3, len(results))):
+        results[i]["llm_explanation"] = generate_explanation(
+            jd_text, texts[i], results[i]
         )
 
     return {"results": results}
 
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
