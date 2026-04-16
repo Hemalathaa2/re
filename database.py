2eb81da252f547b87cd252a6f69968a88061e55e
Hemalathaa2 <155246186+Hemalathaa2@users.noreply.github.com>
2026-04-16 10:33:59 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
 import sqlite3
+import json
 
 def init_db():
     conn = sqlite3.connect("candidates.db")
@@ -12,6 +13,13 @@ def init_db():
         semantic_score REAL,
         skill_score REAL,
         experience_score REAL
+    )
+    """)
+
+    c.execute("""
+    CREATE TABLE IF NOT EXISTS jobs (
+        job_id TEXT PRIMARY KEY,
+        result TEXT
     )
     """)
 
@@ -35,3 +43,26 @@ def insert_result(r):
 
     conn.commit()
     conn.close()
+
+def save_job(job_id, results):
+    conn = sqlite3.connect("candidates.db")
+    c = conn.cursor()
+
+    c.execute("""
+    INSERT OR REPLACE INTO jobs (job_id, result)
+    VALUES (?, ?)
+    """, (job_id, json.dumps(results)))
+
+    conn.commit()
+    conn.close()
+
+def get_job(job_id):
+    conn = sqlite3.connect("candidates.db")
+    c = conn.cursor()
+
+    c.execute("SELECT result FROM jobs WHERE job_id=?", (job_id,))
+    row = c.fetchone()
+
+    conn.close()
+
+    return json.loads(row[0]) if row else None
